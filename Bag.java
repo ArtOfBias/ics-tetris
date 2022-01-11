2a95696b35dfc0d064372b9a1d94b7c87cc30dd8
aris chen <harryc142857@gmail.com>
2022-01-11 17:34:58 -0500

    added bag system class

---
+++ Bag.java
@@ -0,0 +1,38 @@
+public class Bag {
+    private int[] bagPieces = new int[] {1,2,3,4,5,6,7};
+
+    public Bag(){
+        shuffle();
+    }
+
+    public int piece(int index){
+        return bagPieces[index];
+    }
+
+    public void shuffle(){
+        int targetIndex;
+        int temp;
+
+        for (int i = 0; i < 7; i++){
+            targetIndex = randInt(0, 6);
+
+            temp = bagPieces[i];
+            bagPieces[i] = bagPieces[targetIndex];
+            bagPieces[targetIndex] = temp;
+        }
+    }
+
+    // TODO not sure if this is useful
+    public void reset(){
+        bagPieces = new int[] {1,2,3,4,5,6,7};
+    }
+
+    private static int randInt(int a, int b){
+        return (int)Math.floor(randNumber(a, b + 1));
+    }
+
+    private static double randNumber(double a, double b){
+        double x = Math.random();
+        return x * (b - a) + a;
+    }
+}
--- Tetrimino.java
+++ Tetrimino.java
@@ -1,6 +1,3 @@
-// import java.util.Random;
-
-
 class Tetrimino {
     private static final String[] TYPE = new String[] {"o","i","t","l","j","s","z"};
 
@@ -11,88 +8,12 @@ class Tetrimino {
     private int temp;
     private int type;
 
-    public Tetrimino(String type){
-        changeType(type);
-    }
-
     public Tetrimino(int type){
         changeType(type);
     }
 
-    public void changeType(String type){
-        if (type.equals("o")){
-            blocks[0] = new int[] {-1,0};
-            blocks[1] = new int[] {0,0};
-            blocks[2] = new int[] {-1,-1};
-            blocks[3] = new int[] {0,-1};
-            anchorLeft = 0;
-            anchorRight = 1;
-            anchorTurn = 1;
-            this.type = 1;
-        }
-        else if (type.equals("i")){
-            blocks[0] = new int[] {-2,0};
-            blocks[1] = new int[] {-1,0};
-            blocks[2] = new int[] {0,0};
-            blocks[3] = new int[] {1,0};
-            anchorLeft = 1;
-            anchorRight = 2;
-            anchorTurn = 2;
-            this.type = 2;
-        }
-        else if (type.equals("t")){
-            blocks[0] = new int[] {0,1};
-            blocks[1] = new int[] {-1,0};
-            blocks[2] = new int[] {0,0};
-            blocks[3] = new int[] {1,0};
-            anchorLeft = 3;
-            anchorRight = 1;
-            anchorTurn = 2;
-            this.type = 3;
-        }
-        else if (type.equals("l")){
-            blocks[0] = new int[] {1,1};
-            blocks[1] = new int[] {-1,0};
-            blocks[2] = new int[] {0,0};
-            blocks[3] = new int[] {1,0};
-            anchorLeft = 2;
-            anchorRight = 2;
-            anchorTurn = 2;
-            this.type = 4;
-        }
-        else if (type.equals("j")){
-            blocks[0] = new int[] {-1,1};
-            blocks[1] = new int[] {-1,0};
-            blocks[2] = new int[] {0,0};
-            blocks[3] = new int[] {1,0};
-            anchorLeft = 2;
-            anchorRight = 2;
-            anchorTurn = 2;
-            this.type = 5;
-        }
-        else if (type.equals("s")){
-            blocks[0] = new int[] {0,0};
-            blocks[1] = new int[] {1,0};
-            blocks[2] = new int[] {-1,-1};
-            blocks[3] = new int[] {0,-1};
-            anchorLeft = 0;
-            anchorRight = 0;
-            anchorTurn = 0;
-            this.type = 6;
-        }
-        else if (type.equals("z")){
-            blocks[0] = new int[] {0,0};
-            blocks[1] = new int[] {1,0};
-            blocks[2] = new int[] {1,-1};
-            blocks[3] = new int[] {2,-1};
-            anchorLeft = 1;
-            anchorRight = 1;
-            anchorTurn = 1;
-            this.type = 7;
-        }
-        else {
-            throw new IllegalArgumentException("invalid piece type, valid pieces are [o,i,t,l,j,s,z]");
-        }
+    public Tetrimino(String type){
+        changeType(type);
     }
 
     public void changeType(int type){
@@ -168,6 +89,82 @@ class Tetrimino {
         }
         else {
             throw new IllegalArgumentException("invalid piece number, valid pieces numbers are [1,2,3,4,5,6,7]");
+        }
+    }
+
+    public void changeType(String type){
+        if (type.equals("o")){
+            blocks[0] = new int[] {-1,0};
+            blocks[1] = new int[] {0,0};
+            blocks[2] = new int[] {-1,-1};
+            blocks[3] = new int[] {0,-1};
+            anchorLeft = 0;
+            anchorRight = 1;
+            anchorTurn = 1;
+            this.type = 1;
+        }
+        else if (type.equals("i")){
+            blocks[0] = new int[] {-2,0};
+            blocks[1] = new int[] {-1,0};
+            blocks[2] = new int[] {0,0};
+            blocks[3] = new int[] {1,0};
+            anchorLeft = 1;
+            anchorRight = 2;
+            anchorTurn = 2;
+            this.type = 2;
+        }
+        else if (type.equals("t")){
+            blocks[0] = new int[] {0,1};
+            blocks[1] = new int[] {-1,0};
+            blocks[2] = new int[] {0,0};
+            blocks[3] = new int[] {1,0};
+            anchorLeft = 3;
+            anchorRight = 1;
+            anchorTurn = 2;
+            this.type = 3;
+        }
+        else if (type.equals("l")){
+            blocks[0] = new int[] {1,1};
+            blocks[1] = new int[] {-1,0};
+            blocks[2] = new int[] {0,0};
+            blocks[3] = new int[] {1,0};
+            anchorLeft = 2;
+            anchorRight = 2;
+            anchorTurn = 2;
+            this.type = 4;
+        }
+        else if (type.equals("j")){
+            blocks[0] = new int[] {-1,1};
+            blocks[1] = new int[] {-1,0};
+            blocks[2] = new int[] {0,0};
+            blocks[3] = new int[] {1,0};
+            anchorLeft = 2;
+            anchorRight = 2;
+            anchorTurn = 2;
+            this.type = 5;
+        }
+        else if (type.equals("s")){
+            blocks[0] = new int[] {0,0};
+            blocks[1] = new int[] {1,0};
+            blocks[2] = new int[] {-1,-1};
+            blocks[3] = new int[] {0,-1};
+            anchorLeft = 0;
+            anchorRight = 0;
+            anchorTurn = 0;
+            this.type = 6;
+        }
+        else if (type.equals("z")){
+            blocks[0] = new int[] {0,0};
+            blocks[1] = new int[] {1,0};
+            blocks[2] = new int[] {1,-1};
+            blocks[3] = new int[] {2,-1};
+            anchorLeft = 1;
+            anchorRight = 1;
+            anchorTurn = 1;
+            this.type = 7;
+        }
+        else {
+            throw new IllegalArgumentException("invalid piece type, valid pieces are [o,i,t,l,j,s,z]");
         }
     }
 
