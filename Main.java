3f8b932d53e915870f57afbae9ee19e59805a15f
aris chen <harryc142857@gmail.com>
2022-01-17 16:10:32 -0500

    removed testing code

--- Main.java
+++ Main.java
@@ -9,17 +9,6 @@
 
 class Main {
     public static void main(String[] args){
-        Tetrimino a = new Tetrimino("l");
-        a.print();
-        a.rotate("left");
-        a.print();
-        a = new Tetrimino("i");
-        a.print();
-        a.rotate("left");
-        a.print();
-        a.rotate("turn");
-        a.print();
-
         Draw frame = new Draw();
         frame.setVisible(true);
     }
