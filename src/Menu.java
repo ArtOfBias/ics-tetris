d546e0bea3373fd904e7448f9cf33a87e4662c0d
Alvin Young <yalvin58@gmail.com>
2022-01-19 14:03:42 -0500

    basic menu layout

--- src/Menu.java
+++ src/Menu.java
@@ -1,24 +1,36 @@
-/*package src;
+package src;
 
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 public class Menu extends JFrame{
-    
+    JButton play;
+    JButton exit;
+
     public Menu(){
         this.setTitle("Tetris");
         this.setResizable(false);
         this.setBackground(Color.GRAY);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.pack();
+        this.setSize(500, 500);
         this.setVisible(true);
-        this.setLocationRelativeTo(null);
+
+        play = new JButton("Play");
+        exit = new JButton("Exit");
+
+        Container c = getContentPane();
+        c.setLayout(null);
+        play.setSize(100, 50);
+        exit.setSize(100, 50);
+        play.setLocation(200, 200);
+        exit.setLocation(200, 300);
+        c.add(play);
+        c.add(exit);
     }
 
     public void paint(Graphics g){
         super.paint(g);
-        g.drawString("JFrame creates Frames!", 10 , 50);
+        g.setFont(new Font("TimesRoman", Font.PLAIN, 50)); 
+        g.drawString("Tetris", 195, 150);
     }
 }
-
-*/
